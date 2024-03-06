6c54995ac95e7dac1dbead792a0a87f233bbb110
Abdulla Shahil <abdullashahil0077@gmail.com>
2024-03-06 12:27:55 +0530

    create post

--- Back-end/Models/postModel.js
+++ Back-end/Models/postModel.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  postID: Number,
   songTitle: String,
   username: String,
   artist: String,
   releaseYear: Number,
   imageVideo: String,
   genre: String,
-  quirkinessLevel: String
 });
 
 const postModel = mongoose.model('Post', postSchema);
